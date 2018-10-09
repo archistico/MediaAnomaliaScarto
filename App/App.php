602c4813b143f4cbf7edf2fb2d821db4ac6e83d6
archistico <info@archistico.com>
2018-10-09 12:54:36 +0200

    template

--- App/App.php
+++ App/App.php
@@ -3,7 +3,7 @@ namespace App;
 
 class App
 {
-    public function Iniziale()
+    public function Iniziale($f3)
     {
         //$listaValori = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9];
         $listaValori = [2,4];
@@ -11,8 +11,10 @@ class App
         $scartoQuadraticoMedio = \App\Matematica\Media::ScartoQuadraticoMedio($listaValori);
         $mediaSenzaAnomalie = \App\Matematica\Media::MediaSenzaAnomalie($listaValori);
         
-        echo $media . "<br>";
-        echo $scartoQuadraticoMedio . "<br>";
-        echo $mediaSenzaAnomalie . "<br>";
+        $f3->set('media', $media);
+        $f3->set('sqm', $scartoQuadraticoMedio);
+        $f3->set('msa', $mediaSenzaAnomalie);
+
+        echo \Template::instance()->render('template/base.htm');
     }
 }
